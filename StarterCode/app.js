599f1541fdd4b2d5138fa1e295f10a2331e0e4f0
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-02-28 17:41:36 -0500

    debug

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,14 +1,14 @@
 // build the function 
 
-
+console.log("test")
 function GetId (id) {
-d3.json(sampe.json).then((sampleD) => {
+d3.json(samples.json).then((sampleD) => {
     console.log(sampleD)
 
     // Get the Washing frequency
     var frequency = data.metadata.map( md => md.wfreq)
     // Console log to see if it work
-    console.log(`Washing: ${frequency}`)
+    console.log(`${frequency}`)
 }
 )
     
