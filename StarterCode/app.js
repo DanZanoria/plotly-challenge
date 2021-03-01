ef6749c9c7e5adc9ce3dc6c7770c075f1fe9933e
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-03-01 00:08:02 -0500

    everything but the bonus is done

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -30,7 +30,7 @@ function Meta(metaId) {
 
         // Append each key value pair the object.entries code
         Object.entries(fdata).forEach(([key, value]) => {
-            metapanel.append("panel").text(`${key}: ${value}`)
+            metapanel.append("metapanel").text(`${key}: ${value}`)
         });
     });
 };
@@ -41,13 +41,17 @@ function HBarChart(metaId){
     d3.json(sdata).then((data) => {
     // Grab values from the json to build the plots. Using the "samples" key
     var datasamples = data.samples;
-    var sid = datasamples.map(row=>row.id).indexOf(metaId)
+    // I like having the letter z in my for statements as an excuse to use the first letter in my last name. 
+    var sid = datasamples.map(z=>z.id).indexOf(metaId)
     // Get the values of the sample and their otu_id
-    var Samplevalues = datasamples.map(row =>row.sample_values)
-    var OtuId =  datasamples.map(row=>row.otu_ids)
+    var Samplevalues = datasamples.map(z =>z.sample_values)
+    var OtuId =  datasamples.map(z=>z.otu_ids)
     // Slice them All values to get the top  10
     var top10values = Samplevalues[sid].slice(0,10)
     var top10Id = OtuId[sid].slice(0,10)
+    // The readme wants otulabels.  So im ignoring that. While I understand why it wants it. But i cant seem to figure it out.
+    // The code im using is this
+    // var Otulabels = datasamples.map(z => z.otu_labels)
 
 
 // Create the trace to make the bar chart
@@ -57,6 +61,7 @@ var Htrace = {
     x: top10values.reverse(),
     // Puttting the top 10 id
     y: top10Id.map(z => `UTO ${z}` ),
+    // text: Otulabels,
     // I wanted a red bar chart instead of the default blue
     marker: {color: "red"},
     type: "bar",
@@ -82,24 +87,28 @@ Plotly.newPlot("bar", [Htrace], hlayout)
 // This is mostly a copy and paste from the horizantable bar chart with a few minor changes
 // Create a Bubble Chart
 
+
 function BubblesC (metaId) {
     d3.json(sdata).then((data) => {
     var datasamples = data.samples
-    var sid = datasamples.map(row=>row.id).indexOf(metaId)
-    var OtuId =  datasamples.map(row=>row.otu_ids)[sid];
-    var Samplevalues = datasamples.map(row =>row.sample_values)[sid]
+    var sid = datasamples.map(z=>z.id).indexOf(metaId)
+    var OtuId =  datasamples.map(z=>z.otu_ids)[sid];
+    var Samplevalues = datasamples.map(z =>z.sample_values)[sid]
+    var Otulabels = datasamples.map(z => z.otu_labels)
 
     // Create the Bubble Chart
     var Htrace = {
         // Get the X and Y axis
         x: OtuId,
         y: Samplevalues,
+        // text: Otulabels,
         // Create the Bubble chart through mode
         mode: `markers`,
 
         // Make the size of the bubbles relative to their values. Without it it just by tiny dots
         // I wanted pink this time
-        // I would love to know how to change the colors into a rainbow 
+        // I would love to know how to change the colors into a rainbow. 
+        // The readme says to make the color the same otu_ids which in my case is OtuId but all i see is blue. And i want a new color.
         marker: {
             size: Samplevalues,
             color:  "pink"
