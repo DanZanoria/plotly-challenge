3a5c1ff5be3fca9fd5e38e3288ee02321bfd251b
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-02-28 21:21:18 -0500

    hbar

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,10 +1,10 @@
-function init(bacteria) { };
+function init() { };
 
 // Create constant variable to refer the json
 const sdata = "samples.json"
 
 
-// capture the names of te metadata
+// capture the names of te metadata to get the dropdown menu
 function buildPlot() {
     d3.json(sdata).then(function(sampledata){
         var mDataset = d3.select("#selDataset");
@@ -18,5 +18,49 @@ function buildPlot() {
         });
 })};
 
+
+// Create a horizantabl bar chart
+
+function optionChange(changeId){
+    HBarChart(changeId)
+
+};
+
 buildPlot();
-// d3.selectAll("#selDataset").on("change", buildPlot);
+d3.selectAll("#selDataset").on("change", buildPlot);
+
+function HBarChart(metaId){
+    d3.json(sdata).then(function(sampledata){
+    // Grab values from the json to build the plots. We are using the "samples" key
+    var datasamples = data.samples;
+    var sid = datasamples.map(row=>row.id).indexOf(metaId)
+    // Get the values of the sample and their otu_id
+    var Samplevalues = datasamples.map(row =>row.sample_values)
+    var OtuId =  datasamples.map(row=>row.otu_ids)
+    // Slice them All values to get the top  10
+    var top10values = Samplevalues[sid].slice(0,10)
+    var top10OtuId = OtuId[sid].slice(0,10)
+  
+
+// Create the trace to make the bar chart
+
+var Htrace = {
+    x: top10OtuId,
+    y: top10values,
+    type: "bar",
+    orientation: "h"
+
+}
+
+// Create a layout for the bar chart
+
+var hlayout = {
+    title: "Top 10 OTU ID"
+}
+
+// Plot the chart
+Plotly.newPlot("bar", [Htrace], hlayout)
+
+
+});
+};
